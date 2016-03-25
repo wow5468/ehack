65efae47da6a67c95e71885dec6ba0dc0e3caa4e
kimgunhee90 <kimgunhee90@kimgunhee>
2016-03-26 02:59:29 +0900

    asd

---
+++ ehack/src/main/java/ehack/util/RefineData.java
@@ -0,0 +1,28 @@
+package ehack.util;
+
+public class RefineData {
+
+	private String serialNum;
+	public String getSerialNum() {
+		return serialNum;
+	}
+	public void setSerialNum(String serialNum) {
+		this.serialNum = serialNum;
+	}
+	public String getModelNum() {
+		return modelNum;
+	}
+	public void setModelNum(String modelNum) {
+		this.modelNum = modelNum;
+	}
+	public String getIsConnet() {
+		return isConnet;
+	}
+	public void setIsConnet(String isConnet) {
+		this.isConnet = isConnet;
+	}
+	private String modelNum;
+	private String isConnet;
+	
+	
+}
