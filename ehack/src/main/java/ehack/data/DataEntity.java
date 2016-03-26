e8b54c51ac189064c4d80a5e4f76116c1749c22c
wow5468 <wow5468@naver.com>
2016-03-26 11:35:20 +0900

    dsfsdafsadfdsafdsa

--- ehack/src/main/java/ehack/data/DataEntity.java
+++ ehack/src/main/java/ehack/data/DataEntity.java
@@ -18,11 +18,10 @@ public class DataEntity {
 	@Column(name = "muuid",  unique = true, nullable = false)
 	private String muuid;
 	
-	@Id
+
 	@Column(name = "duuid")
 	private String duuid;
-	
-	@Id
+
 	@Column(name = "pid")
 	private int pid;
 	
@@ -50,66 +49,7 @@ public class DataEntity {
 	@Column(name = "end")
 	private String end;
 	
-	public class grandPK implements Serializable {
-		/**
-		 * 
-		 */
-		private static final long serialVersionUID = 1L;
-		private String muuid;
-		private String duuid;
-		private int pid;
-		
-		public grandPK() {
-			
-		}
-		
 
-		public grandPK(String muuid, String duuid, int pid) {
-			this.muuid = muuid;
-			this.duuid = duuid;
-			this.pid = pid;
-		}
-		public String getMuuid() {
-			return muuid;
-		}
-
-
-		public void setMuuid(String muuid) {
-			this.muuid = muuid;
-		}
-
-
-		public String getDuuid() {
-			return duuid;
-		}
-
-
-		public void setDuuid(String duuid) {
-			this.duuid = duuid;
-		}
-
-
-		public int getPid() {
-			return pid;
-		}
-
-
-		public void setPid(int pid) {
-			this.pid = pid;
-		}
-
-
-		
-		
-		public boolean equals(Object obj) {
-	        if (obj == this) return true;
-	        if (!(obj instanceof grandPK)) return false;
-	        if (obj == null) return false;
-	        grandPK pk = (grandPK) obj;
-	        return pk.muuid == muuid && pk.duuid.equals(duuid) && pk.pid == pid;
-	    }
-		
-	}
 
 
 }
